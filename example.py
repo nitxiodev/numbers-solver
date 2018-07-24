ec0cc031b1f787eb1059255b00bfa117b94f8a86
nitxiodev <nitxiodev@gmail.com>
2018-07-24 18:45:35 +0200

    improving readability of script

--- example.py
+++ example.py
@@ -2,8 +2,11 @@ from numbers_solver.src.numbers_backtracking import NumbersSolver
 
 if __name__ == '__main__':
     n = NumbersSolver()
-    solk, iter, ellapsed_time = n.lookup(944, [25, 75, 100, 50, 8, 1])
+    target = 952
+    numbers = [75, 25, 4, 10, 6, 100]
+    solk, iter, ellapsed_time = n.lookup(target, numbers)
     print solk
     print iter
     print '{:.2f}s'.format(ellapsed_time)
-    print len([])
+
+
